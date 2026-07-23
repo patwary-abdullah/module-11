6f72dc28d5f7ac7c5bb835158271f4477d4c9f3c
Patwary-Abdullah <abdullah.acmeai@gmail.com>
2026-07-23 13:08:38 +0600

    Created the repo and push it first time

---
+++ mod11.js
@@ -0,0 +1,16 @@
+const goals =4;
+const player = "Neymar JR";
+const isGolder = true;
+
+if (player === 'Mbappe') {
+    console.log('kire bete');
+}
+
+const matches = ['vs algeria', 'vs belgium'];
+
+
+const cup = "world cup";
+const tea = 'Tea cup';
+const location = `sajek`;
+
+console.log(location.length);
