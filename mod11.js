ea8a69e5ca43e3f89f05262035250739bc9f76a9
Patwary-Abdullah <abdullah.acmeai@gmail.com>
2026-07-23 18:38:38 +0600

    Almost complete the class tasks....

--- mod11.js
+++ mod11.js
@@ -13,4 +13,49 @@ const cup = "world cup";
 const tea = 'Tea cup';
 const location = `sajek`;
 
-console.log(location.length);
+
+// String is immutable, so we cann't cahnge the character of a string
+location[0] = 'w';
+//It does not change
+console.log(location);
+
+
+const name = 'AbdullAh';
+
+console.log (name.toLowerCase());
+console.log (name.toUpperCase());
+
+
+
+// *** Object and function ***
+
+const superShop = {
+    name: "Patwary Trade Centre",
+    location: "Jinjira",
+    mobile: "+880 1896-333749",
+    isOpen: true,
+    'owner name': "Mostafa Patwary"
+}
+
+console.log(superShop);
+
+//  Get and Set
+
+superShop.isOpen=false;
+superShop["Area size:"] = "33 Acre";
+
+console.log(superShop);
+
+console.log(`Get by using dot notation - ${superShop.location}`);
+
+console.log(`Get by using Bracket notation - ${superShop["owner name"]}`);
+
+// Delete an object property
+delete superShop.isOpen;
+
+// Get object keys and values
+console.log(Object.keys(superShop));
+
+console.log(Object.values(superShop));
+
+
